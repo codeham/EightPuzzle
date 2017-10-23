310dba6ceb1122f6284fac327b8a2db0205cf006
cavinalopez <cavinalopez@cpp.edu>
2017-10-22 20:18:28 -0700

    finished both heuristics, come back and test out h2(n)...

--- src/PuzzleDriver.java
+++ src/PuzzleDriver.java
@@ -52,6 +52,7 @@ public class PuzzleDriver {
      * h1(n)
      * Heuristic 1 - calculating misplaced tiles
      * @return int that holds misplaced tile count
+     * subtract 1 since blank space doesn't rep. a move
      */
     public int getMisplacedTiles(){
         int misplacedCount = 0;
@@ -61,18 +62,45 @@ public class PuzzleDriver {
                 misplacedCount++;
             }
         }
+        if(misplacedCount == 0){ return misplacedCount; }
         return misplacedCount-1;
     }
 
     public int getManhattanDistance(){
-        return 0;
+        int[] boardArray = currentBoard.getStateArray();
+        int manhattanSum = 0;
+        for(int i = 0; i < boardArray.length; i++){
+            if(boardArray[i] == 0){ continue; }
+            int boardCol = i % ((boardArray.length/2)-1);
+            int boardRow = (i - boardCol) / ((boardArray.length/2)-1);
+            int tileNumber = boardArray[i];
+            System.out.println("col: " + boardCol + " row: " + boardRow + "    -> " + tileNumber);
+            int[] goalPosition = getGoalPosition(tileNumber);
+            manhattanSum += Math.abs(goalPosition[0] - boardRow) + Math.abs(goalPosition[1] - boardCol);
+        }
+        System.out.println("Manhattan Sum is : " + manhattanSum);
+        return manhattanSum;
+    }
+
+    public int[] getGoalPosition(int tileNumber){
+        int[] position;
+        for(int i = 0; i < goalBoard.length; i++){
+            if(goalBoard[i] == tileNumber){
+                int col = i % ((goalBoard.length/2)-1);
+                int row = (i - col) / ((goalBoard.length/2)-1);
+                position = new int[]{row, col};
+                return position;
+            }
+        }
+        return null;
     }
 
     public void solvePuzzle(){
         if(isSolvable(currentBoard.getStateArray())){ System.out.println("Puzzle is solvable !");
         }else{ System.out.println("Not solvable !");}
-
+        System.out.println();
         System.out.println("Misplaced Tiles: " + getMisplacedTiles());
+        getManhattanDistance();
 
     }
 
